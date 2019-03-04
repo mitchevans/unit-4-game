dabe1f657ba3e147e1c2a71dde991b08f564652e
Mitch <mrmtevans@gmail.com>
2019-03-03 23:23:59 -0800

    working game with reset button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -34,6 +34,7 @@ let enemies = [];
 console.log(enemies.length);
 let defender= [];
 let character = [];
+let defeated= [];
 
 $("#char0").html(
     "<img src='" + characters[0]["picture"]  + "'/>")
@@ -70,8 +71,8 @@ $("#char0").on("click", function() {
         $("#enemyTitle").text("Select an Enemy to Attack");
         $("#characterTitle").text("Character");
         $("#characterStats").html("<p>" + Grant["name"] + "</p>"
-                                + "<p>" + "Attack Power: " + Grant["attackPower"] + "</p>"
-                                + "<p>" + "Health Points: " + Grant["healthPoints"] + "</p>");
+                                + "<p>" + "Attack: " + Grant["attackPower"] + "</p>"
+                                + "<p>" + "Health: " + Grant["healthPoints"] + "</p>");
 }
 })
 
@@ -93,8 +94,8 @@ $("#char1").on("click", function() {
         $("#enemyTitle").text("Select an Enemy to Attack")
         $("#characterTitle").text("Character")
         $("#characterStats").html("<p>" + Ellie["name"] + "</p>"
-                                + "<p>" + "Attack Power: " + Ellie["attackPower"] + "</p>"
-                                + "<p>" + "Health Points: " + Ellie["healthPoints"] + "</p>")
+                                + "<p>" + "Attack: " + Ellie["attackPower"] + "</p>"
+                                + "<p>" + "Health: " + Ellie["healthPoints"] + "</p>")
 }
 })
 
@@ -116,8 +117,8 @@ $("#char2").on("click", function() {
         $("#enemyTitle").text("Select an Enemy to Attack")
         $("#characterTitle").text("Character")
         $("#characterStats").html("<p>" + Hammond["name"] + "</p>"
-                                + "<p>" + "Attack Power: " + Hammond["attackPower"] + "</p>"
-                                + "<p>" + "Health Points: " + Hammond["healthPoints"] + "</p>")
+                                + "<p>" + "Attack: " + Hammond["attackPower"] + "</p>"
+                                + "<p>" + "Health: " + Hammond["healthPoints"] + "</p>")
 }
 })
 
@@ -139,8 +140,8 @@ $("#char3").on("click", function() {
         $("#enemyTitle").text("Select an Enemy to Attack")
         $("#characterTitle").text("Character")
         $("#characterStats").html("<p>" + Malcolm["name"] + "</p>"
-                                + "<p>" + "Attack Power: " + Malcolm["attackPower"] + "</p>"
-                                + "<p>" + "Health Points: " + Malcolm["healthPoints"] + "</p>")
+                                + "<p>" + "Attack: " + Malcolm["attackPower"] + "</p>"
+                                + "<p>" + "Health: " + Malcolm["healthPoints"] + "</p>")
 }
 })
 
@@ -154,8 +155,8 @@ $("#enemy0").on("click", function() {
         $("#defender").html("<img src='" + defender[0]["picture"]  + "'/>") ;
         $("#defenderTitle").html("<h1>" + "Click on " + defender[0]["name"] + " to Attack " + defender[0]["gender"] + "</h1>");
         $("#defenderStats").html("<p>" + defender[0]["name"] + "</p>"
-                                + "<p>" + "Counter Attack Power: " + defender[0]["counterAttackPower"] + "</p>"
-                                + "<p>" + "Health Points: " + defender[0]["healthPoints"] + "</p>");
+                                + "<p>" + "Counter Attack: " + defender[0]["counterAttackPower"] + "</p>"
+                                + "<p>" + "Health: " + defender[0]["healthPoints"] + "</p>");
         }
 })
 
@@ -167,8 +168,8 @@ $("#enemy1").on("click", function() {
         $("#defender").html("<img src='" + defender[0]["picture"]  + "'/>") 
         $("#defenderTitle").html("<h1>" + "Click on " + defender[0]["name"] + " to Attack " + defender[0]["gender"] + "</h1>");
         $("#defenderStats").html("<p>" + defender[0]["name"] + "</p>"
-                                + "<p>" + "Counter Attack Power: " + defender[0]["counterAttackPower"] + "</p>"
-                                + "<p>" + "Health Points: " + defender[0]["healthPoints"] + "</p>");
+                                + "<p>" + "Counter Attack: " + defender[0]["counterAttackPower"] + "</p>"
+                                + "<p>" + "Health: " + defender[0]["healthPoints"] + "</p>");
         }
 })
 
@@ -180,32 +181,196 @@ $("#enemy2").on("click", function() {
         $("#defender").html("<img src='" + defender[0]["picture"]  + "'/>") 
         $("#defenderTitle").html("<h1>" + "Click on " + defender[0]["name"] + " to Attack " + defender[0]["gender"] + "</h1>");
         $("#defenderStats").html("<p>" + defender[0]["name"] + "</p>"
-                                + "<p>" + "Counter Attack Power: " + defender[0]["counterAttackPower"] + "</p>"
-                                + "<p>" + "Health Points: " + defender[0]["healthPoints"] + "</p>");
+                                + "<p>" + "Counter Attack: " + defender[0]["counterAttackPower"] + "</p>"
+                                + "<p>" + "Health: " + defender[0]["healthPoints"] + "</p>");
         }
 })
 
+// attacking first defender
 $("#defender").on("click", function() {
         if (defender[0]["healthPoints"] > 0) {
                 defender[0]["healthPoints"] -= character[0]["attackPower"];
                 character[0]["healthPoints"] -= defender[0]["counterAttackPower"];
-        }
+        
         $("#defenderStats").html("<p>" + defender[0]["name"] + "</p>"
-                                + "<p>" + "Counter Attack Power: " + defender[0]["counterAttackPower"] + "</p>"
-                                + "<p>" + "Health Points: " + defender[0]["healthPoints"] + "</p>");
+                                + "<p>" + "Counter Attack: " + defender[0]["counterAttackPower"] + "</p>"
+                                + "<p>" + "Health: " + defender[0]["healthPoints"] + "</p>");
 
         character[0]["attackPower"] += 10;
 
         $("#characterStats").html("<p>" + character[0]["name"] + "</p>"
-                                + "<p>" + "Attack Power: " + character[0]["attackPower"] + "</p>"
-                                + "<p>" + "Health Points: " + character[0]["healthPoints"] + "</p>")
-
+                                + "<p>" + "Attack: " + character[0]["attackPower"] + "</p>"
+                                + "<p>" + "Health: " + character[0]["healthPoints"] + "</p>")
+        }
+        if (defender[0]["healthPoints"] <= 0) {
+                $("#defenderStats").html("<p>" + defender[0]["name"] + "</p>"
+                + "<p>Has Been</p>" +
+                "<p>Elimnated</p>");
+                $("#enemyTitle").text("Select Another Enemy to Attack");
+                defeated.push(defender[0]);
+        }
 })
 
+$("#enemy0").on("click", function() {
+        if (defeated.length === 1) {
+        $("#enemy0").html("");
+        $("#enemyTitle").text("Enemies to Attack");
+        defender.push(enemies[0]);
+        $("#defender1").html("<img src='" + defender[1]["picture"]  + "'/>") ;
+        $("#defenderTitle").html("<h1>" + "Click on " + defender[1]["name"] + " to Attack " + defender[1]["gender"] + "</h1>");
+        $("#defender1Stats").html("<p>" + defender[1]["name"] + "</p>"
+                                + "<p>" + "Counter Attack: " + defender[1]["counterAttackPower"] + "</p>"
+                                + "<p>" + "Health: " + defender[1]["healthPoints"] + "</p>");
+        }
+})
 
+$("#enemy1").on("click", function() {
+        if (defeated.length === 1) {
+        $("#enemy1").html("");
+        $("#enemyTitle").text("Enemies to Attack");
+        defender.push(enemies[1]);
+        $("#defender1").html("<img src='" + defender[1]["picture"]  + "'/>") ;
+        $("#defenderTitle").html("<h1>" + "Click on " + defender[1]["name"] + " to Attack " + defender[1]["gender"] + "</h1>");
+        $("#defender1Stats").html("<p>" + defender[1]["name"] + "</p>"
+                                + "<p>" + "Counter Attack: " + defender[1]["counterAttackPower"] + "</p>"
+                                + "<p>" + "Health: " + defender[1]["healthPoints"] + "</p>");
+        }
+})
 
+$("#enemy2").on("click", function() {
+        if (defeated.length === 1) {
+        $("#enemy2").html("");
+        $("#enemyTitle").text("Enemies to Attack");
+        defender.push(enemies[2]);
+        $("#defender1").html("<img src='" + defender[1]["picture"]  + "'/>") ;
+        $("#defenderTitle").html("<h1>" + "Click on " + defender[1]["name"] + " to Attack " + defender[1]["gender"] + "</h1>");
+        $("#defender1Stats").html("<p>" + defender[1]["name"] + "</p>"
+                                + "<p>" + "Counter Attack: " + defender[1]["counterAttackPower"] + "</p>"
+                                + "<p>" + "Health " + defender[1]["healthPoints"] + "</p>");
+        }
+})
 
+// attacking second defender
+        $("#defender1").on("click", function() {
+                console.log(defender[1]);
+                console.log(defender[1]["healthPoints"]);
 
-
-
+                if (defender[1]["healthPoints"] > 0) {
+                        defender[1]["healthPoints"] -= character[0]["attackPower"];
+                        character[0]["healthPoints"] -= defender[1]["counterAttackPower"];
+                
+                $("#defender1Stats").html("<p>" + defender[1]["name"] + "</p>"
+                                        + "<p>" + "Counter Attack: " + defender[1]["counterAttackPower"] + "</p>"
+                                        + "<p>" + "Health: " + defender[1]["healthPoints"] + "</p>");
         
+                character[0]["attackPower"] += 10;
+        
+                $("#characterStats").html("<p>" + character[0]["name"] + "</p>"
+                                        + "<p>" + "Attack: " + character[0]["attackPower"] + "</p>"
+                                        + "<p>" + "Health: " + character[0]["healthPoints"] + "</p>")
+                }
+                if (defender[1]["healthPoints"] <= 0) {
+                        $("#defender1Stats").html("<p>" + defender[1]["name"] + "</p>"
+                        + "<p>Has Been</p>" +
+                        "<p>Elimnated</p>");
+                        $("#enemyTitle").text("Select Another Enemy to Attack");
+                        defeated.push(defender[1]);
+                }
+        }) 
+// selecting last enemy
+$("#enemy0").on("click", function() {
+        if (defeated.length === 2) {
+        $("#enemy0").html("");
+        $("#enemyTitle").text("");
+        defender.push(enemies[0]);
+        $("#defender2").html("<img src='" + defender[2]["picture"]  + "'/>") ;
+        $("#defenderTitle").html("<h1>" + "Click on " + defender[2]["name"] + " to Attack " + defender[2]["gender"] + "</h1>");
+        $("#defender2Stats").html("<p>" + defender[2]["name"] + "</p>"
+                                + "<p>" + "Counter Attack: " + defender[2]["counterAttackPower"] + "</p>"
+                                + "<p>" + "Health: " + defender[2]["healthPoints"] + "</p>");
+                }
+        })
+
+$("#enemy1").on("click", function() {
+        if (defeated.length === 2) {
+        $("#enemy1").html("");
+        $("#enemyTitle").text("");
+        defender.push(enemies[1]);
+        $("#defender2").html("<img src='" + defender[2]["picture"]  + "'/>") ;
+        $("#defenderTitle").html("<h1>" + "Click on " + defender[2]["name"] + " to Attack " + defender[2]["gender"] + "</h1>");
+        $("#defender2Stats").html("<p>" + defender[2]["name"] + "</p>"
+                                + "<p>" + "Counter Attack: " + defender[2]["counterAttackPower"] + "</p>"
+                                + "<p>" + "Health: " + defender[2]["healthPoints"] + "</p>");
+                }
+        })
+$("#enemy2").on("click", function() {
+        if (defeated.length === 2) {
+        $("#enemy2").html("");
+        $("#enemyTitle").text("");
+        defender.push(enemies[2]);
+        $("#defender2").html("<img src='" + defender[2]["picture"]  + "'/>") ;
+        $("#defenderTitle").html("<h1>" + "Click on " + defender[2]["name"] + " to Attack " + defender[2]["gender"] + "</h1>");
+        $("#defender2Stats").html("<p>" + defender[2]["name"] + "</p>"
+                                + "<p>" + "Counter Attack: " + defender[2]["counterAttackPower"] + "</p>"
+                                + "<p>" + "Health: " + defender[2]["healthPoints"] + "</p>");
+                }
+        })
+// attacking final defender
+$("#defender2").on("click", function() {
+        console.log(defender[2]);
+        console.log(defender[2]["healthPoints"]);
+
+        if (defender[2]["healthPoints"] > 0) {
+                defender[2]["healthPoints"] -= character[0]["attackPower"];
+                character[0]["healthPoints"] -= defender[2]["counterAttackPower"];
+        
+        $("#defender2Stats").html("<p>" + defender[2]["name"] + "</p>"
+                                + "<p>" + "Counter Attack: " + defender[2]["counterAttackPower"] + "</p>"
+                                + "<p>" + "Health: " + defender[2]["healthPoints"] + "</p>");
+
+        character[0]["attackPower"] += 10;
+
+        $("#characterStats").html("<p>" + character[0]["name"] + "</p>"
+                                + "<p>" + "Attack: " + character[0]["attackPower"] + "</p>"
+                                + "<p>" + "Health: " + character[0]["healthPoints"] + "</p>")
+        }
+        if (defender[2]["healthPoints"] <= 0) {
+                $("#defender2Stats").html("<p>" + defender[2]["name"] + "</p>"
+                + "<p>Has Been</p>" +
+                "<p>Elimnated</p>");
+                $("#enemyTitle").text("");
+                defeated.push(defender[2]);
+                $("#defenderTitle").html("<h1>Congratulations You Defeated Every Enemy</h1>");
+                $("#reset").html("<h1>Reset</h1>")
+        }
+}) 
+
+$("#reset").on("click", function() {let characters = [Grant, Ellie, Hammond, Malcolm];
+        enemies = [];
+        defender= [];
+        character = [];
+        defeated= [];
+        
+        $("#char0").html(
+            "<img src='" + characters[0]["picture"]  + "'/>")
+        
+        $("#char1").html(
+                "<img src='" + characters[1]["picture"]  + "'/>")
+        
+        $("#char2").html(
+                "<img src='" + characters[2]["picture"]  + "'/>")
+                
+        $("#char3").html(
+                "<img src='" + characters[3]["picture"]  + "'/>")
+        $("#characterTitle").text("Select a Character")
+        $("#characterStats").html("");
+        $("#defender").html("");
+        $("#defenderStats").html("");
+        $("#defender1").html("");
+        $("#defender1Stats").html("");
+        $("#defender2").html("");
+        $("#defender2Stats").html("");
+        $("#defenderTitle").html("");
+        $("#reset").html("");
+
+})
